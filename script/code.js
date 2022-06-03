3d84414dfac3b7c200a85022f663bbf18db77cf4
Meagan Williams <meaganwilliams407@gmail>
2022-06-03 14:49:07 +0200

    first

---
+++ script/code.js
@@ -0,0 +1,41 @@
+// Initialization
+// let numb1 = 9;
+
+// console.log(numb1);
+// let numb2 = 7;
+// console.log(numb1 + numb2);
+
+// let studentNumber = 87;
+
+// let numb1 = 4;
+// let numb2 = 2;
+// console.log(numb1 > numb2);
+// console.log(numb1 ** numb2);
+// numb1++;
+// console.log(numb1++);
+// numb1+=5;
+// console.log(numb1);
+// console.log(++numb1);
+
+// let age = 18;
+// let salary = 5000;
+// console.log((age >= 18) && (salary >= 5000));
+
+/* 
+>: Greater than
+<: Less than
+>=: Greater or equal to
+<=: Less or equal to
+// =: Not equal to
+==: Equal to
+===: Check both value and
+data type
+ */
+
+// var myStr = "I am a \"double quoted\" string inside another \"double quotes\"."; 
+// console.log(myStr);
+
+let firstName = 'Peter';
+let lastName = 'Hank';
+let age = 17;
+console.log("My name is " .concat(firstName,",\nSurname is ",lastName ,",\nAge is ",age," years old"));
